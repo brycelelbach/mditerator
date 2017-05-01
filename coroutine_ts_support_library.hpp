fe5802d2908e3062b9b75326f1011a35c67b3fb9
Bryce Adelstein-Lelbach aka wash <brycelelbach@lbl.gov>
2017-04-30 22:30:26 -0700

    Add copyright info I forgot to copy over from the split into multiple files.

--- coroutine_ts_support_library.hpp
+++ coroutine_ts_support_library.hpp
@@ -1,5 +1,6 @@
 ///////////////////////////////////////////////////////////////////////////////
 // Copyright (c) 2015-7 Bryce Adelstein Lelbach aka wash <brycelelbach@gmail.com>
+// Copyright (c) 2017 Gor Nishanov
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
